b347fe336cee80917a52f1481080135d383febab
Devendraappa <devendraappa.kune@gmail.com>
2024-12-24 14:37:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,66 @@
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Web Server SG allow SSH & HTTP Ports"
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # For testing; restrict to your IP for production.
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all out bound ports to all destinations"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web_server" {
+  ami           = var.aws_ami
+  instance_type = var.instance_type
+  key_name      = "guru"
+
+  security_groups = [ aws_security_group.web_sg.name ]
+
+  tags = {
+    "Name"      = "Web_Server"
+    "ManagedBy" = "IaC"
+  }
+
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = self.public_ip
+      private_key = file("./desktop.pem")
+    }
+
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras enable nginx1",
+      "sudo yum install -y nginx",
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx"
+    ]
+    on_failure = continue
+  }
+
+}
