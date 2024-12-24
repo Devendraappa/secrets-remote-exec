fc6862a0070c9c4f7b3587269b4d86c150847249
Devendraappa <devendraappa.kune@gmail.com>
2024-12-24 14:54:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_instance" "web_server" {
       "sudo apt-get update -y",
       "sudo apt-get install -y nginx",
       "sudo systemctl start nginx",
-      "sudo systemctl enable nginx"
+      "sudo systemctl enable nginx",
       "sudo systemctl status nginx"
     ]
     on_failure = continue
